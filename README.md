ea763e9eba3da5688e85536f90cf1494bc25e139
ayyysh04 <ayushiit2003@gmail.com>
2023-09-12 22:28:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# invoice_app_sih
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/invoice_app_sih/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.invoice_app_sih
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
